ddc93398cf7f1c3c2508f7e11103fa4b9ff689eb
DableArt <104915738+DableArt@users.noreply.github.com>
2026-05-06 20:14:27 +0300

    PauseInput

--- Assets/Scripts/UI/PauseInputHandler.cs
+++ Assets/Scripts/UI/PauseInputHandler.cs
@@ -1,20 +1,15 @@
 using R3;
 using UnityEngine;
 
-/// <summary>
-/// Слушает нажатие Escape и публикует событие через Observable.
-/// </summary>
 public class PauseInputHandler : MonoBehaviour
 {
-    // Внешние подписчики могут слушать это событие
-    public Observable<Unit> OnEscapePressed => _escapeSubject;
-
-    private readonly Subject<Unit> _escapeSubject = new Subject<Unit>();
+    public Observable<bool> OnEscapePressed => _escapeSubject;
+    private readonly Subject<bool> _escapeSubject = new Subject<bool>();
 
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
-            _escapeSubject.OnNext(Unit.Default);
+            _escapeSubject.OnNext(true);
     }
 
     private void OnDestroy()
